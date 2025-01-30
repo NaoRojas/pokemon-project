4ff3e4cc2700f3968d375cfc9c50ac122ccaa983
naoreds <naorojash@gmail.com>
2025-01-29 19:35:51 -0600

    Refactor getPokemonListByType function to return typed PokemonListItem array; improve data mapping and structure

--- api/pokeApi.ts
+++ api/pokeApi.ts
@@ -1,4 +1,6 @@
 import { toCapitalize } from './../lib/toCapitalize';
+import { PokemonListItem } from '@/types/Pokemon';
+
 const POKEMON_API = "https://pokeapi.co/api/v2/";
 
 export async function getPokemonList() {
@@ -24,14 +26,14 @@ export async function getPokemonType() {
     return data.results;
 }
 
-export async function getPokemonListByType(type: string) {
+export async function getPokemonListByType(type: string): Promise<PokemonListItem[]> {
     const response = await fetch(POKEMON_API + "type/" + type);
-    const data = await response.json().then((res) => {
-        return res.pokemon.map(({ pokemon }) => ({
+    const data = await response.json();
+    return data.pokemon.map(({ pokemon }: { pokemon: { name: string, url: string } }) => ({
+        pokemon: {
             name: pokemon.name,
-            detailName: toCapitalize(pokemon.name),
-            url: pokemon.url
-        }))
-    });
-    return data;
+            url: pokemon.url,
+            detailName: toCapitalize(pokemon.name)
+        }
+    }));
 }
